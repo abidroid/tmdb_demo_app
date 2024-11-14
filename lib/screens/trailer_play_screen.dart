46a7f5dbb236d00e489eba0fc3ab0f68c14eaf20
Muhammad Abid <abidroid1@gmail.com>
2024-11-14 22:22:39 +0500

    trailer play screen

--- lib/screens/trailer_play_screen.dart
+++ lib/screens/trailer_play_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:tmdb_demo_app/blocs/trailer/trailer_bloc.dart';
+import 'package:tmdb_demo_app/models/trailer_model.dart';
 
 class TrailerPlayScreen extends StatefulWidget {
   final int movieId;
@@ -22,7 +24,33 @@ class _TrailerPlayScreenState extends State<TrailerPlayScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Placeholder(),
+      body: BlocBuilder<TrailerBloc, TrailerState>(builder: (context, state) {
+        if (state is TrailerLoading) {
+          return const Center(
+            child: SpinKitDualRing(color: Colors.blue),
+          );
+        }
+
+        if (state is TrailerError) {
+          return Center(
+            child: Text(state.message),
+          );
+        }
+
+        if (state is TrailerLoaded) {
+          TrailerModel? trailerModel = state.trailerModel;
+
+          if (trailerModel == null) {
+            return const Center(
+              child: Text('Couldn\'t load movie trailer'),
+            );
+          }
+
+          // return youtube player
+        }
+
+        return const SizedBox.shrink();
+      }),
     );
   }
 }
