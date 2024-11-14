371155a51ed0190ac6c4904e69e704a4cc94127e
Muhammad Abid <abidroid1@gmail.com>
2024-11-14 10:32:20 +0500

    screens

---
+++ lib/screens/trailer_play_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TrailerPlayScreen extends StatefulWidget {
+  const TrailerPlayScreen({super.key});
+
+  @override
+  State<TrailerPlayScreen> createState() => _TrailerPlayScreenState();
+}
+
+class _TrailerPlayScreenState extends State<TrailerPlayScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Placeholder(),
+    );
+  }
+}
