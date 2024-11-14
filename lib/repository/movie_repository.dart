c0ed76f45038893d09b1b6e476a1023768b67ae8
Muhammad Abid <abidroid1@gmail.com>
2024-11-14 11:01:37 +0500

    movie repo

--- lib/repository/movie_repository.dart
+++ lib/repository/movie_repository.dart
@@ -1,5 +1,31 @@
+import 'dart:convert';
+
+import 'package:tmdb_demo_app/models/upcoming_movie_model.dart';
+import 'package:http/http.dart' as http;
 
 class MovieRepository {
+  final apiKey = "e28a4c17944e58f941e810e99e67c6a0";
+  final baseUrl = "https://api.themoviedb.org/3";
 
-  
+  Future<List<UpcomingMovieModel>?> getUpcomingMovies() async {
+    http.Response response =
+        await http.get(Uri.parse("$baseUrl/movie/upcoming?$apiKey"));
+
+    if (response.statusCode == 200) {
+      List<UpcomingMovieModel> upcomingMovies = [];
+
+      var jsonResponse = jsonDecode(response.body);
+
+      var results = jsonResponse['results'];
+
+      for (var result in results) {
+        UpcomingMovieModel upcomingMovieModel =
+            UpcomingMovieModel.fromJson(result);
+        upcomingMovies.add(upcomingMovieModel);
+      }
+      return upcomingMovies;
+    } else {
+      return null;
+    }
+  }
 }
